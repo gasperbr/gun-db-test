5821a4c369e80030afbde085649faea6822a782c
unknown <gasper.brvar@gmail.com>
2021-08-09 15:04:27 +0200

    gun db test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Gun from 'gun/gun'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'gun-db-test';
+
+  gun: any;
+
+  inT = "";
+  outT = "";
+  inA = 0;
+  outA = 0;
+
+  metaTx: any;
+
+  data: any[] = [];
+
+  constructor() {
+    this.gun = Gun(['https://my-gun-db-test.herokuapp.com/gun', 'https://mvp-gun.herokuapp.com/gun', 'https://e2eec.herokuapp.com/gun']);
+    this.metaTx = this.gun.get('metaTx');
+    this.metaTx.on((entry: any) => {
+      this.data.push({ ...entry });
+    });
+  }
+
+  addOrder(): void {
+    this.metaTx.put({
+      inToken: this.inT,
+      outToken: this.outT,
+      inAmount: this.inA,
+      outAmount: this.outA
+    });
+  }
+
 }
